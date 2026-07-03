5a485e4e933cd867f6b48576fb6fbb187726d8cc
priviledgencube005-ops <priviledgencube005@gmail.com>
2026-07-03 15:11:37 +0200

    Add files via upload

---
+++ grade checker.py
@@ -0,0 +1,17 @@
+marks = int(input("Enter your marks: "))
+
+if marks >= 80:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+elif marks >= 50:
+    print("Grade D")
+else:
+    print("Grade F")
+
+
+
+
+  
